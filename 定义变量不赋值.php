55eaa4d9d6d37a1d6befb574d7de843eff425bc5
xueli233 <250263603@qq.com>
2017-11-30 11:32:50 +0800

    变量不赋值

---
+++ 定义变量不赋值.php
@@ -0,0 +1,14 @@
+<?php
+  $a;
+  // var_dump($a==0);
+  // var_dump($a==false);
+  // var_dump($a=='');
+  // var_dump($a==null);
+  // var_dump($a===0);
+  // var_dump($a===false);
+  // var_dump($a==='');
+  // var_dump($a===null);
+  // echo $a;
+  // var_dump(empty($a));
+  // echo( ++$a);
+  // var_dump( ++$a);
