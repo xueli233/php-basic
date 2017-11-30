a60d5813323fac5190faba683dec46e05615c8fa
xueli233 <250263603@qq.com>
2017-11-30 17:05:06 +0800

    数组长度 20171130/17:05:06

--- 数组长度.php
+++ 数组长度.php
@@ -9,5 +9,5 @@ array_push($a[0],'a','5');
 // 0(或COUNT_NORMAL)为默认,不检测多维数组(数组中的数组);
 // 1(或COUNT_RECURSIVE)为检测多维数组，
 var_dump($a);
-var_dump(count($a,1));
+var_dump(count($a,1)); 
 var_dump(count(null));
